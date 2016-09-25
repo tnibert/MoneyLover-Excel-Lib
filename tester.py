7ca1fd7a970eaf20f11fd7c09c213d578e5a8ed3
Tim <docz2a@gmail.com>
2016-09-25 15:37:10 +1000

    tested sortByDate() function and it worked!

--- tester.py
+++ tester.py
@@ -2,14 +2,15 @@ from moneylover import *
 #import calculations as calc
 
 #sortByCategory("test.xlsx")
-lsClass, hdr = loadMLWorkbook("test.xlsx")
+lsClass, hdr = loadMLWorkbook("catsorted.xlsx")
 hdr.display()
 #for x in lsClass:
 #	x.display()
 lsList = generateArray(lsClass)
 #print lsList
-sortedClassList = sortByCategory(lsClass)
+#sortedClassList = sortByCategory(lsClass)
+sortedClassList = sortByDate(lsClass)
 #for x in sortedClassList:
 #       x.display()
-exportToNewWorkbook(hdr, sortedClassList, "testexport.xlsx")
+exportToNewWorkbook(hdr, sortedClassList, "datesorted.xlsx")
 #calc.addByCategory()
