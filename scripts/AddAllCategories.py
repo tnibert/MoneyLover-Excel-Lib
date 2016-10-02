139ea74c672cf409dc03f0c04f0c629687718f14
Tim <docz2a@gmail.com>
2016-10-02 17:04:08 +1100

    created AddAllCategories script

---
+++ scripts/AddAllCategories.py
@@ -0,0 +1,22 @@
+from moneylover import *
+import sys
+
+try:
+	fname = sys.argv[1]
+except:
+	print "call with python AddAllCategories.py FILENAME"
+	exit()
+
+
+lsClass, hdr = loadMLWorkbook("category.xlsx")
+
+separatedcats = spliceByCategory(lsClass)
+for x in separatedcats:
+	if len(x) == 0:
+		print "EMPTY"
+	else: 
+		amount = 0
+		print x[0].category + " contains " + str(len(x)) + " entries"
+		for y in x:
+			amount += y.amount
+		print "Amount: " + str(amount) + "\n"
