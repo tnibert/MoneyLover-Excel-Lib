dca58a3a338a9039907a9881dc43f8dee5836791
Tim Nibert <docz2a@gmail.com>
2017-06-09 16:30:59 +1000

    began creating tally function

--- calculations.py
+++ calculations.py
@@ -2,10 +2,10 @@ from moneylover import *
 
 #take a list and add together all of the amounts in each category
 #return a hash table
-def addAmountByCategory(mlRowList):
+#def addAmountByCategory(mlRowList):
 	#making a design decision between taking a presorted list or sorting it on the fly
 	#for now we sacrifice cpu cycles so others can be lazy
-	sortedList = sortByCategory(mlRowList)
+#	sortedList = sortByCategory(mlRowList)
 
 	#compare row's category with the previous row's category
 	#if they are the same, add together
@@ -13,6 +13,9 @@ def addAmountByCategory(mlRowList):
 	#and begin anew
 
 	#have to pop first element off of sortedList
-	for row in sortedList:
-		
-	return
+#	for row in sortedList:
+
+#	return
+
+def tallyCategories(rowlist, startdate=NULL, enddate=NULL):
+    return
