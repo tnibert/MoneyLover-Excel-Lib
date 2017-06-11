aedb7589048ec854109d87cebe67586ee85cdafb
Tim Nibert <docz2a@gmail.com>
2017-06-12 01:06:09 +1000

    small clean up and organization

--- calculations.py
+++ calculations.py
@@ -2,26 +2,10 @@ from moneylover import *
 
 #this file has more high level functions for tasks
 
-#take a list and add together all of the amounts in each category
-#return a hash table
-#def addAmountByCategory(mlRowList):
-	#making a design decision between taking a presorted list or sorting it on the fly
-	#for now we sacrifice cpu cycles so others can be lazy
-#	sortedList = sortByCategory(mlRowList)
-
-	#compare row's category with the previous row's category
-	#if they are the same, add together
-	#if they are different, push the previous added value into the hash w/ category name
-	#and begin anew
-
-	#have to pop first element off of sortedList
-#	for row in sortedList:
-
-#	return
-
 #take a rowlist and start and end dates as args, return a dict of category:amount
 def tallyCategories(rowlist, startdate=-1, enddate=-1):
 #still need to implement date splicing
+#still need to document
 
     #separate out list by categories
     separatedcats = spliceByCategory(rowlist)
